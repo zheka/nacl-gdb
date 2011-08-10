edec58f6d5bcbacc660afefbd17acb759938a6d7
Evgeny Eltsin <eaeltsin@chromium.org>
2011-08-10 21:45:27 +0400

    Add nacl program loaded event to solib events

--- gdb/solib-nacl.c
+++ gdb/solib-nacl.c
@@ -18,9 +18,44 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "defs.h"
+#include "breakpoint.h"
+#include "gdbcore.h"
 #include "solib.h"
 #include "solib-svr4.h"
 #include "solist.h"
+
+
+/* Native Client loader calls "_ovly_debug_event" right after the program
+   is loaded into memory.  Report this as a solib event.  */
+
+static void
+nacl_enable_break (void)
+{
+  struct minimal_symbol *solib_event_sym;
+
+  solib_event_sym = lookup_minimal_symbol ("_ovly_debug_event", NULL, NULL);
+  if (solib_event_sym)
+    {
+      CORE_ADDR addr;
+
+      addr = SYMBOL_VALUE_ADDRESS (solib_event_sym);
+      addr = gdbarch_convert_from_func_ptr_addr (target_gdbarch,
+                                                 addr,
+                                                 &current_target);
+      create_solib_event_breakpoint (target_gdbarch, addr);
+    }
+}
+
+
+static void
+nacl_solib_create_inferior_hook (int from_tty)
+{
+  /* Call SVR4 hook -- this will re-insert the SVR4 solib breakpoints.  */
+  svr4_so_ops.solib_create_inferior_hook (from_tty);
+
+  nacl_enable_break ();
+}
+
 
 void
 set_nacl_solib_ops (struct gdbarch *gdbarch)
@@ -32,6 +67,7 @@ set_nacl_solib_ops (struct gdbarch *gdbarch)
   if (nacl_so_ops.current_sos == NULL)
     {
       nacl_so_ops = svr4_so_ops;
+      nacl_so_ops.solib_create_inferior_hook = nacl_solib_create_inferior_hook;
     }
 
   set_solib_ops (gdbarch, &nacl_so_ops);
