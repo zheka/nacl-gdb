db8066b100ff27d14f57075594d2b734d8e47ee0
Evgeny Eltsin <eaeltsin@chromium.org>
2011-08-11 20:56:55 +0400

    Add function to check for nacl bfd

---
+++ gdb/nacl-tdep.c
@@ -0,0 +1,31 @@
+/* Target-dependent code for Native Client.
+
+   Copyright (C) 2011 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "defs.h"
+#include "elf-bfd.h"
+
+int
+nacl_bfd_p (bfd *abfd)
+{
+  if (bfd_get_flavour (abfd) == bfd_target_elf_flavour &&
+      elf_elfheader (abfd)->e_ident[EI_OSABI] == 123)
+    return 1;
+
+  return 0;
+}
