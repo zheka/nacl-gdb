0897369b86b15b96a7b377734a2ab175bbda3510
Evgeny Eltsin <eaeltsin@chromium.org>
2011-11-16 18:10:08 +0400

    Mechanically transfer from nacl multiarch to target beneath

--- gdb/nacl-multiarch.c
+++ gdb/nacl-multiarch.c
@@ -18,9 +18,115 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "defs.h"
+#include "inferior.h"
+
+
+static struct target_ops nacl_ops;
+
+
+static void
+nacl_mourn_inferior (struct target_ops *ops)
+{
+  struct target_ops *ops_beneath = find_target_beneath (ops);
+  while (ops_beneath && !ops_beneath->to_mourn_inferior)
+    ops_beneath = find_target_beneath (ops_beneath);
+  gdb_assert (ops_beneath);
+  ops_beneath->to_mourn_inferior (ops_beneath);
+}
+
+
+static void
+nacl_fetch_registers (struct target_ops *ops, struct regcache *regcache, int regno)
+{
+  struct target_ops *ops_beneath = find_target_beneath (ops);
+  while (ops_beneath && !ops_beneath->to_fetch_registers)
+    ops_beneath = find_target_beneath (ops_beneath);
+  gdb_assert (ops_beneath);
+  ops_beneath->to_fetch_registers (ops_beneath, regcache, regno);
+}
+
+
+static void
+nacl_store_registers (struct target_ops *ops, struct regcache *regcache, int regno)
+{
+  struct target_ops *ops_beneath = find_target_beneath (ops);
+  while (ops_beneath && !ops_beneath->to_store_registers)
+    ops_beneath = find_target_beneath (ops_beneath);
+  gdb_assert (ops_beneath);
+  ops_beneath->to_store_registers (ops_beneath, regcache, regno);
+}
+
+
+static LONGEST
+nacl_xfer_partial (struct target_ops *ops, enum target_object object,
+		   const char *annex, gdb_byte *readbuf,
+		   const gdb_byte *writebuf, ULONGEST offset, LONGEST len)
+{
+  struct target_ops *ops_beneath = find_target_beneath (ops);
+  while (ops_beneath && !ops_beneath->to_xfer_partial)
+    ops_beneath = find_target_beneath (ops_beneath);
+  gdb_assert (ops_beneath);
+  return ops_beneath->to_xfer_partial (ops_beneath, object, annex, readbuf, writebuf, offset, len);
+}
+
+
+static int
+nacl_search_memory (struct target_ops* ops,
+		    CORE_ADDR start_addr, ULONGEST search_space_len,
+		    const gdb_byte *pattern, ULONGEST pattern_len,
+		    CORE_ADDR *found_addrp)
+{
+  struct target_ops *ops_beneath = find_target_beneath (ops);
+  while (ops_beneath && !ops_beneath->to_search_memory)
+    ops_beneath = find_target_beneath (ops_beneath);
+  gdb_assert (ops_beneath);
+  return ops_beneath->to_search_memory (ops_beneath, start_addr, search_space_len, pattern, pattern_len, found_addrp);
+}
+
+
+static int
+nacl_region_ok_for_hw_watchpoint (CORE_ADDR addr, int len)
+{
+  struct target_ops *ops_beneath = find_target_beneath (&nacl_ops);
+  while (ops_beneath && !ops_beneath->to_region_ok_for_hw_watchpoint)
+    ops_beneath = find_target_beneath (ops_beneath);
+  gdb_assert (ops_beneath);
+  return ops_beneath->to_region_ok_for_hw_watchpoint (addr, len);
+}
+
+
+static struct gdbarch *
+nacl_thread_architecture (struct target_ops *ops, ptid_t ptid)
+{
+  struct target_ops *ops_beneath = find_target_beneath (ops);
+  while (ops_beneath && !ops_beneath->to_thread_architecture)
+    ops_beneath = find_target_beneath (ops_beneath);
+  gdb_assert (ops_beneath);
+  return ops_beneath->to_thread_architecture (ops_beneath, ptid);
+}
+
+
+static void
+init_nacl_ops (void)
+{
+  nacl_ops.to_shortname = "nacl";
+  nacl_ops.to_longname = "NaCl multi-architecture support.";
+  nacl_ops.to_doc = "NaCl multi-architecture support.";
+  nacl_ops.to_mourn_inferior = nacl_mourn_inferior;
+  nacl_ops.to_fetch_registers = nacl_fetch_registers;
+  nacl_ops.to_store_registers = nacl_store_registers;
+  nacl_ops.to_xfer_partial = nacl_xfer_partial;
+  nacl_ops.to_search_memory = nacl_search_memory;
+  nacl_ops.to_region_ok_for_hw_watchpoint = nacl_region_ok_for_hw_watchpoint;
+  nacl_ops.to_thread_architecture = nacl_thread_architecture;
+  nacl_ops.to_stratum = arch_stratum;
+  nacl_ops.to_magic = OPS_MAGIC;
+}
 
 
 void
 _initialize_nacl_multiarch (void)
 {
+  init_nacl_ops ();
+  add_target (&nacl_ops);
 }
